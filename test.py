e5d6118b606fe6e30b038ff80867ee5caf74a25e
bsvdango <dangopython@gmail.com>
2019-11-30 14:10:33 +0530

    Added 'Point' class

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-# test
+class Point(object):
+    def __init__(self, x = 0.0, y = 0.0):
+        self.m_x = x
+        self.m_y = y
+    def print(self):
+        print(self.m_x, self.m_y)
+
 if __name__ == "__main__":
     import tkinter
     tkinter._test()
+    P1 = Point(45,78)
+    P1.print()
 
